a3b4d555b2ddd6f25fa8663862b9f86e505342a7
Antoine Parat <antoineparat@MacBook-Air-de-Antoine.local>
2019-07-16 23:04:06 +0200

    test3

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 require("./mongoDB/mongoose")
 const express = require("express");
+const history = require('connect-history-api-fallback');
 const userRouter = require('./routes/users');
 const storeRouter = require('./routes/stores')
 const cookieParser = require('cookie-parser');
-// const history = require('connect-history-api-fallback');
+
 
 const app = express();
+app.use(history());
+
 const port = process.env.PORT || 3000; 
 
 app.use(cookieParser());
-
-// app.use(history());
 
 /* UNCOMMENT FOR PRODUCTION */
 app.use(express.static(__dirname + '/public/' ));
