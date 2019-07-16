4dcbb02d7e324889a0525f07b7f951822a5d8896
Antoine Parat <antoineparat@MacBook-Air-de-Antoine.local>
2019-07-16 22:50:26 +0200

    test4

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const userRouter = require('./routes/users');
 const storeRouter = require('./routes/stores')
 const cookieParser = require('cookie-parser');
-const history = require('connect-history-api-fallback');
+// const history = require('connect-history-api-fallback');
 
 const app = express();
 const port = process.env.PORT || 3000; 
 
 app.use(cookieParser());
 
-app.use(history());
+// app.use(history());
 
 /* UNCOMMENT FOR PRODUCTION */
 app.use(express.static(__dirname + '/public/' ));
