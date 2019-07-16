8f4c9e1442f0e068a2831fe6fcef5060fa5b2e4b
Antoine Parat <antoineparat@MacBook-Air-de-Antoine.local>
2019-07-16 23:14:13 +0200

    test3

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const cookieParser = require('cookie-parser');
 
 
 const app = express();
-app.use(history());
+app.use(history({
+    verbose: true
+}));
 
 const port = process.env.PORT || 3000; 
 
