cc8158abff1821d7af391d15a057bf129d03027e
Antoine Parat <antoineparat@MacBook-Air-de-Antoine.local>
2019-07-17 11:10:45 +0200

    test5

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 app.use(cookieParser());
 
 /* UNCOMMENT FOR PRODUCTION */
-// app.use(express.static(__dirname + '/public/' ));
+app.use(express.static(__dirname + '/public/' ));
 //  app.get('*', (req,res) => res.sendFile(__dirname + '/public/index.html'))
 
 app.use(express.json()) //automatically parse upcoming JSON file
